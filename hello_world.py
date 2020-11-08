4999d8f6f1030ec3c26a4b3ecbbef8aece978163
steve_global <18079940168@163.com>
2020-11-08 18:52:47 +0800

    v1.0

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# D:\Users\Administrator\Anaconda3\python.exe
+# -*- coding: UTF-8 -*-
+# @Author  : Steve
+# @File    : hello_world.py
+# @Software: PyCharm
+# @Time    : 2020-11-08 下午 6:51
+
+print("hello world")
