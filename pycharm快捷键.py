17bcddff4686add7fcaa01f41de6ef315ffd8e23
steve_global <18079940168@163.com>
2020-11-08 18:08:33 +0800

    pycharm 快捷键

---
+++ pycharm快捷键.py
@@ -0,0 +1,60 @@
+# ctrl + D 快速复制上行内容
+
+# ctrl + J 输入常用代码
+
+# ctrl + shift + J将两行合并为一行，并删除不必要的空格以符合您的代码样式
+
+# ctrl + P 查看函数参数
+
+# ctrl + R 替换
+
+# ctrl + H 查看当前类的继承树
+
+# ctrl + Q 快速查看库文档
+
+# ctrl + E 查看|最近的文件
+
+# ctrl + 字母O 重写父类方法
+
+# ctrl + Alt + S 快速进入设置页面
+
+# ctrl + Alt 提前方法
+
+# ctrl + Alt + L 一键 PEP8
+
+# Shift + F6 文件快速重命名或 将 重命名代码中变量名
+
+# 打开.py文件后鼠标在标签上右键可以左右上下分屏，在scrapy项目中有奇效
+
+# ctrl + shift + "+-" 快速折叠代码
+
+# ctrl + shift + U 变量名大小写转换,不选中变量名会装换整个文件中的字母
+
+# ctrl + shift + V 调出剪贴板中的历史
+
+# ctrl + shift + 回车自动结束代码，行末自动添加分号（用于if、while、with)
+
+# alt + 上下方向键 快速跳转到方法中
+
+# Alt + Shift + C快速查看您最近对项目的更改
+
+# ctrl + alt + left方向键 跳转到最后编辑处
+
+# ctrl + [ / ] 跳转上下代码块
+
+# (重点)菜单栏 run->profile[xxx.py] 一键进行代码性能分析
+
+# （重点）ctrl + alt + T 或 Shift + alt + Z 调出 Sorround with 模板选项
+
+# （重点）ctrl + alt + M 将选中代码封装成一个函数
+
+# Shift + F11 展开所有书签
+
+# （重点）ctrl + F11 添加书签
+
+# （重点）ctrl + shift + i 快速预览源码
+
+# （重点）ctrl + shift +字母O 调出翻译界面
+
+# (重点)shift + alt + E  运行选定的代码
+
