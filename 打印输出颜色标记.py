0eefc65f9aaae8ae39acf2ee7bccd9231837e0f2
steve_global <18079940168@163.com>
2020-11-08 17:59:28 +0800

    这是后面添加的普通py文件

---
+++ 打印输出颜色标记.py
@@ -0,0 +1,9 @@
+print(
+    "\033[5;36;40m -请求成功 http://avmoo.host/cn/star/7a0f306f6f8e9b4f -次\033[;;m"
+
+)
+print(
+"\033[11;36;40m---请求成功 http://avmoo.host/cn/star/7a0f306f6f8e9b4f--次\033[;;m"
+)
+print("\033[1;34;43m 56\033[;;m")
+print('\033[0;31;44m',"输出颜色测试\033[;;m")
